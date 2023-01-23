5feeae9c549de0c02e1c250fcd6563c42fccf827
Adalidh1 <adalidh141@gmail.com>
2023-01-23 17:05:57 -0600

    v3

--- src/components/Cartas.js
+++ src/components/Cartas.js
@@ -7,74 +7,93 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { v4 as uuid } from 'uuid';
 
-function Cartas(){
+function Cartas() {
 
     const initialState = {
-        'id' : '',
-        'nombre' : '',
-        'direccion' : '',
-        'telefono' : '',
+        'id': '',
+        'nombre': '',
+        'direccion': '',
+        'telefono': '',
     }
 
     const [datos, setDatos] = useState(initialState);
-    
-    const {nombre, direccion, telefono} = datos; //deconstruccion de datos
 
-    const handleSubmit = (event) =>{
+    const { nombre, direccion, telefono } = datos; //deconstruccion de datos
+
+    const [informacion, setInformacion] = useState([]);
+
+    const handleEliminar = (e) => {
+        console.log(e.target.name);
+    }
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("hola mundo");
+        const initialState2 = {
+            'id': uuid(),
+            'nombre': datos.nombre,
+            'direccion': datos.direccion,
+            'telefono': datos.telefono,
+        };
+        let inf = informacion;
+        inf.push(initialState2);
+        setInformacion(inf);
+        setDatos(initialState);
+        console.log(initialState);
     }
 
     const handleChange = (e) => {
         setDatos({
-            ...datos, [e.target.name] : e.target.value
+            ...datos, [e.target.name]: e.target.value
         });
-        console.log(datos);
     }
-    
-    return(        
+
+    return (
         <Container>
-            <Row>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
+            <Row className="row-cols-3">
+                {
+                    informacion.map(inf => (
+                        <Col key={inf.id} className='mt-3'>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Card.Title>{inf.nombre}</Card.Title>
+                                    <Card.Text>id: {inf.id}</Card.Text>
+                                    <Card.Text>Dirección: {inf.direccion}</Card.Text>
+                                    <Card.Text>Teléfono: {inf.telefono}</Card.Text>
+                                    <Button name={'e' + inf.id} variant="danger" className="me-3" onClick={handleEliminar}>Eliminar</Button>
+                                    <Button name={'m' + inf.id} variant="info">Modificar</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))
+                }
             </Row>
 
-       <Row>
-        <Col></Col>
-        <Col>
-            <Form onSubmit={ handleSubmit }>
-                <Form.Group className="mb-3" controlId="nombre">
-                    <Form.Label>Nombre</Form.Label>
-                    <Form.Control type="text" name="nombre" placeholder="Ingresa tu nombre" value={nombre} onChange={handleChange} />
-                </Form.Group>
+            <Row>
+                <Col></Col>
+                <Col>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="nombre">
+                            <Form.Label>Nombre</Form.Label>
+                            <Form.Control type="text" name="nombre" placeholder="Ingresa tu nombre" value={nombre} onChange={handleChange} />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="direccion">
-                    <Form.Label>Dirección</Form.Label>
-                    <Form.Control type="text" name="direccion" placeholder="Ingresa tu direccion" value={direccion} onChange={handleChange} />
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="direccion">
+                            <Form.Label>Dirección</Form.Label>
+                            <Form.Control type="text" name="direccion" placeholder="Ingresa tu direccion" value={direccion} onChange={handleChange} />
+                        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="telefono">
-                    <Form.Label>Telefono</Form.Label>
-                    <Form.Control type="text" name="telefono" placeholder="Ingresa tu telefono" value={telefono} onChange={handleChange} />
-                </Form.Group>
+                        <Form.Group className="mb-3" controlId="telefono">
+                            <Form.Label>Telefono</Form.Label>
+                            <Form.Control type="text" name="telefono" placeholder="Ingresa tu telefono" value={telefono} onChange={handleChange} />
+                        </Form.Group>
 
-                <Button variant="primary" type="submit" name="btnAgregar" >
-                    Agregar
-                </Button>
-            </Form>
-            </Col>
-            <Col></Col>
-        </Row>
+                        <Button variant="primary" type="submit" name="btnAgregar" >
+                            Agregar
+                        </Button>
+                    </Form>
+                </Col>
+                <Col></Col>
+            </Row>
         </Container>
     );
 }
